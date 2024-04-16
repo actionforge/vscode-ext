66be8d6650437b9d243ee0cdc87471f52bf9dd54
Sebastian Rath <git@sebastianrath.io>
2024-04-15 22:31:45 -0400

    Fix indentation

--- src/editor.ts
+++ src/editor.ts
@@ -259,7 +259,7 @@ jobs:
       name: My workflow
       steps:
         - name: Execute Action Graph
-		  uses: actionforge/action@5636645834e4a75cbc8c6c204e872ddc27632738 # v0.9.52
+          uses: actionforge/action@5636645834e4a75cbc8c6c204e872ddc27632738 # v0.9.52
           with:
             graph_file: ${newName}`;
 
